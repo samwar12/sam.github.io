c454f8c946b3f516de14be2c630ff9d841792c55
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-30 11:16:45 +0700

    het video 3

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ function playStream(idVideoTag, stream) {
 const peer = new Peer({ key: 'tkv5g2acaree9udi' });
 
 peer.on('open', id => $('#my-peer').append(id));
+
+//Caller
+$('#btnCall').click(() => {
+    const id = $('#remoteId').val();
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
+
+//Callee
+peer.on('call', call => {
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream', stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
